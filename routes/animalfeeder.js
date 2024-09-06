e014b9fa11896348ad8eb22ad06f4ecd5f6b0b48
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-09-06 23:28:30 +0330

    asdf

--- routes/animalfeeder.js
+++ routes/animalfeeder.js
@@ -4,11 +4,20 @@ var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res, next) => {
-    res.render('./animalfeeder/animalfeeder',{
-        theme: req.session.theme,
-        lang: req.session.lang,
-        user: req.user
-    });
+    const animalFeederData = {
+        time: [
+            { hour: 10, minute: 0, second: 0 },
+            { hour: 0, minute: 0, second: 5 },
+        ],
+        id: 10,
+    };
+    res.json(animalFeederData);
+
+    // res.render('./animalfeeder/animalfeeder',{
+    //     theme: req.session.theme,
+    //     lang: req.session.lang,
+    //     user: req.user
+    // });
 });
 
 module.exports = router;
