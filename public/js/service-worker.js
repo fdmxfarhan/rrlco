8762f439571d42d23e622df85a7a56f8b486fb0c
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-10-03 23:29:07 +0330

    asdf

---
+++ public/js/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+});
+  
+self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+});
+
+  
