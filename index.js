62c97e0a99d111a88fddb40bf35fdce9370cb544
Farhan Daemi <fdmxfarhan@yahoo.com>
2026-02-09 00:02:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.use(function(err, req, res, next) {
 });
 
 var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
+var httpsServer = https.createServer(credentials, app);
 
 // Initialize Socket.IO server
 // ----------------------------------NOTE: For ESP8266 install version 2.x -> npm install socket.io@2.4.1
@@ -195,9 +195,9 @@ http_wss.on('connection', (ws) => io_callback(ws));
 httpServer.listen(3000, () => {
     console.log('http server is started :)')
 });
-// httpsServer.listen(443, () => {
-//     console.log('https server is started :)')
-// });
+httpsServer.listen(443, () => {
+    console.log('https server is started :)')
+});
 
 
 
